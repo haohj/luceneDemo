057402dea62d19d1daedf9c5eaef94ff89fdfe99
haohj <haohj94@163.com>
2018-08-29 14:41:55 +0800

    项目初始化

--- src/main/java/com/hao/lucene/SearchTest.java
+++ src/main/java/com/hao/lucene/SearchTest.java
@@ -1,4 +1,60 @@
 package com.hao.lucene;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
 public class SearchTest {
+    private Directory directory;
+    private IndexReader reader;
+    private IndexSearcher searcher;
+
+    @Before
+    public void setUp() throws Exception {
+        directory = FSDirectory.open(Paths.get("D:\\lucene"));
+        reader = DirectoryReader.open(directory);
+        searcher = new IndexSearcher(reader);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        reader.close();
+    }
+
+    /**
+     * 对特定项搜索
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testTermQuery() throws Exception {
+        String searchField = "contents";
+        String q = "particular";
+        Term term = new Term(searchField, q);
+        Query query = new TermQuery(term);
+        TopDocs topDocs = searcher.search(query, 10);
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            Document doc = searcher.doc(scoreDoc.doc);
+            System.out.println(doc.get("fullPath"));
+        }
+    }
+
+    /**
+     * 解析查询表达式
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testQueryParser() throws Exception {
+
+    }
 }
